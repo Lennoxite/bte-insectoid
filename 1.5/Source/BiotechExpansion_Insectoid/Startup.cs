af4be58d0b596cdd7cb6492bc750e909c8ab2a9a
Lennoxicon <pinoy1999@gmail.com>
2024-03-27 20:05:22 -0700

    Initial commit

---
+++ 1.5/Source/BiotechExpansion_Insectoid/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace BTE_IST
+{
+    [StaticConstructorOnStartup]
+    public static class BTE_Insectoid
+    {
+        static BTE_Insectoid()
+        {
+            Log.Message("Biotech Expansion - Insectoid Loaded!");
+        }
+    }
+}
