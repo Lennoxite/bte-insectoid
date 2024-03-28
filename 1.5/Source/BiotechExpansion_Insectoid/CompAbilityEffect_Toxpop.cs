dbcf0e4d5f9dd2998f40b25dfb199833d739eaee
Lennoxicon <pinoy1999@gmail.com>
2024-03-28 02:04:21 -0700

    megaspider xenotype

---
+++ 1.5/Source/BiotechExpansion_Insectoid/CompAbilityEffect_Toxpop.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+namespace BTE_IST
+{
+    public class CompAbilityEffect_Toxpop : CompAbilityEffect
+    {
+        public new CompProperties_AbilityToxpop Props => (CompProperties_AbilityToxpop)props;
+
+        public bool ShouldHaveInspectString
+        {
+            get
+            {
+                if (ModsConfig.BiotechActive)
+                {
+                    return parent.pawn.RaceProps.IsMechanoid;
+                }
+                return false;
+            }
+        }
+
+        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+            base.Apply(target, dest);
+            GenExplosion.DoExplosion(target.Cell, parent.pawn.MapHeld, Props.smokeRadius, DamageDefOf.Smoke, null, -1, -1f, null, null, null, null, null, 0f, 1, GasType.ToxGas);
+        }
+
+        public override void DrawEffectPreview(LocalTargetInfo target)
+        {
+            GenDraw.DrawRadiusRing(target.Cell, Props.smokeRadius);
+        }
+
+        public override string CompInspectStringExtra()
+        {
+            if (ShouldHaveInspectString)
+            {
+                if (parent.CanCast)
+                {
+                    return "AbilityMechSmokepopCharged".Translate();
+                }
+                return "AbilityMechSmokepopRecharging".Translate(parent.CooldownTicksRemaining.ToStringTicksToPeriod());
+            }
+            return null;
+        }
+
+
+
+    }
+}
---
+++ 1.5/Source/BiotechExpansion_Insectoid/CompProperties_AbilityToxpop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace BTE_IST
+{
+    public class CompProperties_AbilityToxpop : CompProperties_AbilityEffect
+    {
+        public float smokeRadius;
+
+        public CompProperties_AbilityToxpop()
+        {
+            compClass = typeof(CompAbilityEffect_Toxpop);
+        }
+    }
+}
