507ded63d3d6c984492ebd3ab1adeb68183dffb4
Lennoxicon <pinoy1999@gmail.com>
2024-03-28 17:22:18 -0700

    jelly lover...

---
+++ 1.5/Source/BiotechExpansion_Insectoid/BTEIst_HediffDefOf.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using Verse;
+
+namespace BTE_IST
+{
+	[DefOf]
+	public static class BTEIst_HediffDefOf
+	{
+		// Token: 0x0600956F RID: 38255 RVA: 0x00362ADE File Offset: 0x00360CDE
+		static BTEIst_HediffDefOf()
+		{
+			DefOfHelper.EnsureInitializedInCtor(typeof(BTEIst_HediffDefOf));
+		}
+
+		public static HediffDef BTEIst_JellyEnergized;
+
+
+    }
+}
---
+++ 1.5/Source/BiotechExpansion_Insectoid/Gene_JellyLover.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace BTE_IST
+{
+	class Gene_JellyLover : Gene
+	{
+		public override void Notify_IngestedThing(Thing thing, int numTaken)
+		{
+			if (thing.def == ThingDefOf.InsectJelly)
+			{
+				this.pawn.health.AddHediff(BTEIst_HediffDefOf.BTEIst_JellyEnergized);
+			}	
+		}
+	}
+}
