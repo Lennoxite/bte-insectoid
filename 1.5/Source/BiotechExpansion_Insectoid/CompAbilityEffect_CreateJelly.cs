d468c6d377205a1ff992a5523a5147292aa91521
Lennoxicon <pinoy1999@gmail.com>
2024-04-01 23:49:05 -0700

    megascarab

---
+++ 1.5/Source/BiotechExpansion_Insectoid/CompAbilityEffect_CreateJelly.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+namespace BTE_IST
+{
+    public class CompAbilityEffect_CreateJelly : CompAbilityEffect
+    {
+        public new CompProperties_AbilityCreateJelly Props => (CompProperties_AbilityCreateJelly)props;
+
+        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+            base.Apply(target, dest);
+            var jelly = GenSpawn.Spawn(ThingDefOf.InsectJelly, parent.pawn.Position, parent.pawn.Map);
+            jelly.stackCount = Props.amount;
+        }
+
+
+
+
+    }
+}
