dc136caa1cd7af7feee4efe63e8a80625e01f3aa
Lennoxicon <pinoy1999@gmail.com>
2024-04-09 01:03:29 -0700

    jelly transmutation

---
+++ 1.5/Source/BiotechExpansion_Insectoid/CompAbilityEffect_CreateJellyFromThing.cs
@@ -0,0 +1,64 @@
+﻿using RimWorld;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace BTE_IST
+{
+    public class CompAbilityEffect_CreateJellyFromThing : CompAbilityEffect
+    {
+        public new CompProperties_AbilityCreateJellyFromThing Props => (CompProperties_AbilityCreateJellyFromThing)props;
+
+        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+            base.Apply(target, dest);
+            Thing jelly = CreateJelly(target.Thing);
+
+            while (jelly.stackCount > jelly.def.stackLimit)
+            {
+                jelly.stackCount -= jelly.def.stackLimit;
+                var nJelly = GenSpawn.Spawn(ThingDefOf.InsectJelly, parent.pawn.Position, parent.pawn.Map, WipeMode.VanishOrMoveAside);
+                nJelly.stackCount = jelly.def.stackLimit;
+            }
+
+            target.Thing.Destroy();
+        }
+
+        public override bool Valid(LocalTargetInfo target, bool throwMessages = false)
+        {
+            Thing thing = target.Thing;
+            if (thing == null)
+            {
+                return false;
+            }
+            if (!thing.def.IsNutritionGivingIngestible)
+            {
+                if (throwMessages)
+                    Messages.Message("Target isn't an edible food.", thing, MessageTypeDefOf.RejectInput, historical: false);
+                return false;
+            }
+
+            return base.Valid(target, throwMessages);
+        }
+
+        public Thing CreateJelly(Thing target)
+        {
+            float stack = Props.fraction*(target.stackCount * target.def.ingestible.CachedNutrition)/0.05f;
+
+            var jelly = GenSpawn.Spawn(ThingDefOf.InsectJelly, parent.pawn.Position, parent.pawn.Map, WipeMode.VanishOrMoveAside);
+            jelly.stackCount = Mathf.FloorToInt(stack);
+            return jelly;
+
+        }
+
+
+
+
+    }
+}
---
+++ 1.5/Source/BiotechExpansion_Insectoid/CompProperties_AbilityCreateJellyFromThing.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTE_IST
+{
+    public class CompProperties_AbilityCreateJellyFromThing : CompProperties_AbilityEffect
+    {
+        //public int amount = 15;
+        public float fraction = 0.6f;
+        public CompProperties_AbilityCreateJellyFromThing()
+        {
+            compClass = typeof(CompAbilityEffect_CreateJellyFromThing);
+        }
+    }
+}
